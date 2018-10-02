a76788c1f982c49167b900059981966b5ec5c223
saman <saman.ashrafi10@gmail.com>
2018-10-02 11:35:01 +0330

    config server and api users (test)

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const app = express();
+
+// route api
+const users = require("./routes/api/users");
+
+// body parser mildde ware
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const db = require("./config/keys.js").mongoURL;
+
+// connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("mongooDB Connected"))
+  .catch(err => console.log(err));
+
+// Password Middleware
+app.use(passport.initialize());
+
+// Password Config
+require("./config/passport")(passport);
+
+//Use Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server running on port " + port));
