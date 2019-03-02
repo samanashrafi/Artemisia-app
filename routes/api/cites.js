319fcac3c2fb61c603de5600fba1033072c30748
saman <saman.ashrafi10@gmail.com>
2019-03-02 13:40:41 +0330

    [Task] api Cites "Create"

--- routes/api/cites.js
+++ routes/api/cites.js
@@ -1,8 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
-// const Cites = require("../../models/City");
+const validateCity = require("../../validation/cite-valid");
+const City = require("../../models/City");
 
 router.get("/test", (req, res) => res.json({ msg: "Cites works" }));
+
+router.post("/create", (req, res) => {
+  City.findOne({ title: req.body.title }).then(city => {
+    const { errors, isValid } = validateCity(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    if (city) {
+      errors.title = "این شهر قبلا ثبت شده است";
+      return res.status(400).json(errors);
+    } else {
+      const newCity = new City({
+        title: req.body.title,
+        titleEn: req.body.titleEn
+      });
+
+      newCity
+        .save()
+        .then(() => {
+          res.status(200).json({ msg: "شهر با موفقیت ثبت شد." });
+        })
+        .catch(err =>
+          res.status(400).json({
+            errosMsg: "سیستم با خطا مواجه شده است لطفا بعدا امتحان فرمایید."
+          })
+        );
+    }
+  });
+});
 
 module.exports = router;
